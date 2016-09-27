6646766c0e8c4c04785d2f5e4dfca4c807d2d4cd
Mrigesh Priyadarshi <mrigeshpriyadarshi@gmail.com>
2016-09-27 14:31:41 +0800

    BWC env variable script

--- bwc_setenv.bat
+++ bwc_setenv.bat
@@ -2,6 +2,6 @@
 set ST2HOSTNAME="bwcvagrant"
 set ST2BOX='bento/ubuntu-14.04'
 set ST2USER="st2admin"
-set ST2PASSWORD="test"
+set ST2PASSWORD="Ch@ngeMe"
 set BWC_LICENSE="6ahsads6taccjgyactghacjh6tycgbacg6cgjhcctdcb"
 set BWC_SUITES=true
--- bwc_setenv.sh
+++ bwc_setenv.sh
@@ -3,6 +3,6 @@
 export ST2HOSTNAME="bwcvagrant"
 export ST2BOX='bento/ubuntu-14.04'
 export ST2USER="st2admin"
-export ST2PASSWORD="test"
+export ST2PASSWORD="Ch@ngeMe"
 export BWC_LICENSE="6ahsads6taccjgyactghacjh6tycgbacg6cgjhcctdcb"
 export BWC_SUITES=true

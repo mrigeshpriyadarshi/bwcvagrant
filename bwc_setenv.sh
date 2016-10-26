c5cd9b2c2e14a0d20fb86207719e3cb7d3e8e4fb
Mrigesh Priyadarshi <mrigeshpriyadarshi@gmail.com>
2016-10-26 14:42:22 +0800

    added packs installation variable

--- bwc_setenv.sh
+++ bwc_setenv.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#BWC Environment Variables
+#BWC Environment Variables for execution
 export ST2HOSTNAME="bwcvagrant"
 export ST2BOX='bento/ubuntu-14.04'
 export ST2USER="st2admin"
 export ST2PASSWORD="Ch@ngeMe"
-export BWC_LICENSE="6ahsads6taccjgyactghacjh6tycgbacg6cgjhcctdcb"
+export BWC_LICENSE=""
 export BWC_SUITES=true
 export ST2IPADDRESS="192.168.16.20"
+
+export BWC_PACKS="docker vdx icx"

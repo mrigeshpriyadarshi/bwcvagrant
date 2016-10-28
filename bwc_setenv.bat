fffe89b48669f55371c7b53a8bb38f8a33997910
Mrigesh Priyadarshi <mrigeshpriyadarshi@gmail.com>
2016-10-28 16:37:07 +0800

    fixes for pack iteration

--- bwc_setenv.bat
+++ bwc_setenv.bat
@@ -1,8 +1,11 @@
+
 #BWC Environment Variables
 set ST2HOSTNAME="bwcvagrant"
 set ST2BOX='bento/ubuntu-14.04'
 set ST2USER="st2admin"
 set ST2PASSWORD="Ch@ngeMe"
-set BWC_LICENSE="6ahsads6taccjgyactghacjh6tycgbacg6cgjhcctdcb"
+set BWC_LICENSE=""
 set BWC_SUITES=true
 set ST2IPADDRESS="192.168.16.20"
+
+export BWC_PACKS="docker vdx icx"
